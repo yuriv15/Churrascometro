b20ff5eb7e604a8c048a07f2a2ea23d191e67647
Yuri Gonçalves <65895389+yuriv15@users.noreply.github.com>
2021-06-26 02:16:28 -0300

    Primeiro commit
    Já está com a ferramenta funcional.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Churrascometro
  Calculadora para definir a quantidade de carne e bebidas para um churrasco
+
+# Calculo feito a partir dos seguintes dados:
+
+Carne - 400g por pessoa / + de 6 horas - 650
+Cerveja - 1200ml por pessoa / + de 6 horas - 2000ml
+Refrigerante / água - 1000 ml por pessoa / + de 6 horas 1500ml
+Crianças valem por 0.5 
+
+#Sinta-se livre para sugerir melhorias, ainda estou aprendendo então o aprendizado é bem-vindo.
---
+++ script.js
@@ -0,0 +1,33 @@
+// Carne - 400g por pessoa / + de 6 horas - 650
+// Cerveja - 1200ml por pessoa / + de 6 horas - 2000ml
+// Refrigerante / água - 1000 ml por pessoa / + de 6 horas 1500ml
+// Crianças valem por 0.5 
+
+var botao = document.getElementById("botao");
+botao.addEventListener("click", calcular);
+
+function calcular() {
+    let adultos = document.getElementById("adultos").value;
+    let criancas = document.getElementById("criancas").value;
+    let duracao = document.getElementById("duracao").value;
+    let carne = 0;
+    let cerveja = 0;
+    let refri_agua = 0;
+    if (duracao <= 6) {
+        carne = 0.4 * adultos + 0.4 * 0.5 * criancas;
+        cerveja = 1.2 * adultos + 1.2 * 0.5 * criancas;
+        refri_agua = adultos + 0.5 * criancas;
+    } else {
+        carne = 0.65 * adultos + 0.65 * 0.5 * criancas;
+        cerveja = 2 * adultos + 2 * 0.5 * criancas;
+        refri_agua = 1.5 * adultos + 1.5 * 0.5 * criancas;
+    }
+
+    let qtd_carne = document.getElementById("qtd_carne");
+    let qtd_cerveja = document.getElementById("qtd_cerveja");
+    let qtd_bebida = document.getElementById("qtd_bebida");
+
+    qtd_carne.innerHTML = "Quantidade de carne: " + carne + " Kg";
+    qtd_cerveja.innerHTML = "Quantidade de cerveja: " + cerveja + " L";
+    qtd_bebida.innerHTML = "Quantidade de água/refri: " + refri_agua + " L"
+}
